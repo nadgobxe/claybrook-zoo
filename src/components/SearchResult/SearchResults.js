eefbc67036da3b31eb02cb34fbf7a318d525471f
nadgobxe <bogdan.burcea22@my.northampton.ac.uk>
2023-12-21 12:54:13 +0000

    latest

--- src/components/SearchResult/SearchResults.js
+++ src/components/SearchResult/SearchResults.js
@@ -3,6 +3,7 @@ import { useLocation } from "react-router-dom";
 import {animalDataItems} from "../Animals/animalDataItems";
 import { NavLink } from "react-router-dom";
 
+
 export default function SearchResult() {
 
     const location = useLocation();
@@ -18,6 +19,8 @@ export default function SearchResult() {
       )
 
     return     <>
+
+
     <div>Your search results:</div>
     {filterAnimal.map(animal => (
       <div key={animal.name}>Your animal is: <NavLink to={animal.to}>{animal.name}</NavLink> <img src={animal.img} alt={animal.name} width="100vw"/></div>
